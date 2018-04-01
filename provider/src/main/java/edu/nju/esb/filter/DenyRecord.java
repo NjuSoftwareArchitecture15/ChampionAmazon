723543c44ab7f7e63728cb1855c036828ac54c2c
ByronDong <2104869365@qq.com>
2018-04-01 20:53:13 +0800

    实现ESB模块（除了main模块，，应该是基本上实现）

--- provider/src/main/java/edu/nju/esb/filter/DenyRecord.java
+++ provider/src/main/java/edu/nju/esb/filter/DenyRecord.java
@@ -6,34 +6,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DenyRecord {
-	List denyList;
-	
-	private DenyRecord(){
-		denyList=new ArrayList();
-	}
-	
-	private static DenyRecord instance;
-	
-	public void recordDeny(Request request){
-		
-	}
-	
-	public void handleDeny(){
-		
-	}
-	
-	
-	public static DenyRecord getInstance(){
-		if(instance==null){
-			synchronized(DenyRecord.class){
-				if(instance==null){
-					instance=new DenyRecord();
-				}
-			}
-			
-		}
-		
-		return instance;
-	}
+
+    List denyList;
+
+    private DenyRecord() {
+        denyList = new ArrayList();
+    }
+
+    private static DenyRecord instance;
+
+    public void recordDeny(Request request) {
+
+    }
+
+    public void handleDeny() {
+
+    }
+
+    public static DenyRecord getInstance() {
+        if (instance == null) {
+            synchronized (DenyRecord.class) {
+                if (instance == null) {
+                    instance = new DenyRecord();
+                }
+            }
+
+        }
+
+        return instance;
+    }
 
 }
