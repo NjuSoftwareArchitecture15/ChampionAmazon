2ebe2f1281eb45e785d00aafd0306a12d8b357e1
ByronDong <2104869365@qq.com>
2018-04-02 10:09:43 +0800

    与HeartBeatChecker的关遗漏

--- provider/src/main/java/edu/nju/serviceImpl/commodity/WishListServiceImpl.java
+++ provider/src/main/java/edu/nju/serviceImpl/commodity/WishListServiceImpl.java
@@ -1,9 +1,17 @@
 package edu.nju.serviceImpl.commodity;
 
+import edu.nju.monitor.heartbeat.HeartbeatChecker;
 import edu.nju.po.WishList;
 import edu.nju.service.commodity.WishListService;
 
 public class WishListServiceImpl implements WishListService {
+
+    private HeartbeatChecker heartbeatChecker;
+
+    public WishListServiceImpl() {
+        heartbeatChecker = HeartbeatChecker.getInstance();
+    }
+
     public boolean addCart(WishList wishList) {
         return false;
     }
